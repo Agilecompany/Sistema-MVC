27d558b721bb5b443633d499ecb1af379ab85017
AgileCompany <54320945+Agilecompany@users.noreply.github.com>
2020-07-06 19:16:59 -0300

    Atualização Versão 2
    Versão 2

---
+++ TCM_1/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCM_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroFunc()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroProdutos()
+        {
+            return View();
+        }
+
+        public ActionResult Pagamento()
+        {
+            return View();
+        }
+
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+
+        public ActionResult Produtos()
+        {
+            return View();
+        }
+
+        public ActionResult EditCliente()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+    }
+}
