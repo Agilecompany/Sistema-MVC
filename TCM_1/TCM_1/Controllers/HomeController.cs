f252b2ef7db8296ebe69ebc6aca3a141e4b141b6
AgileCompany <54320945+Agilecompany@users.noreply.github.com>
2020-07-06 19:43:21 -0300

    Teste1
    Teste para versionamento

---
+++ TCM_1/TCM_1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCM_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroFunc()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Produtos()
+        {
+            return View();
+        }
+
+        public ActionResult Pagamento()
+        {
+            return View();
+        }
+
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
