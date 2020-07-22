75911abc84887ddcbd3b1bf177173b5d2ea602c0
AgileCompany <54320945+Agilecompany@users.noreply.github.com>
2020-07-22 00:34:09 -0300

    Sistema de autenticação

--- TCM_1/Controllers/HomeController.cs
+++ TCM_1/Controllers/HomeController.cs
@@ -1,26 +1,41 @@
 ﻿using MySql.Data.MySqlClient;
 using ProjetoTCM.Business.Security;
+using ProjetoTCM.Infrastructure.Common.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TCM_1.Models;
 
 namespace TCM_1.Controllers
 {
     public class HomeController : Controller
-    {    
+    {
 
         public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult LoginEnter(string login, string password)
+        public ActionResult LoginEnter(LoginModel loginModel)
         {
-            var result = new UserBusiness().Authentication(login, password);
+            var result = new UserBusiness().Authentication(loginModel.Login, loginModel.Password);
+
+            if (result.Reply == EnumReply.Success)
+            {
+                Session["UserCredential"] = new UserCredentialModel() { Login = loginModel.Login };
+                //Session["UserCredential"] = null;
+            }
 
             return View(result);
+        }
+
+        public ActionResult Logoff()
+        {
+            Session["UserCredential"] = null;
+
+            return View("Login");
         }
 
         public ActionResult Sobre()
---
+++ TCM_1/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCM_1.Models
+{
+    public class LoginModel
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
