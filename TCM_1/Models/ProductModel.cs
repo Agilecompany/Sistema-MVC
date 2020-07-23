8369495a60d464e838b3d4b6571d3c8de26b7762
AgileCompany <54320945+Agilecompany@users.noreply.github.com>
2020-07-22 23:22:35 -0300

    test

--- TCM_1/Models/ProductModel.cs
+++ TCM_1/Models/ProductModel.cs
@@ -7,7 +7,19 @@ namespace TCM_1.Models
 {
     public class ProductModel
     {
-      
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string UnitPrice { get; set; }
+
+        public string ExternalCode { get; set; }
+
+
 
     }
 }
